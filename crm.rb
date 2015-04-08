82fa5d93b61f34d383e7e3a3eea90311cd8815b0
Dan Comand <dcomand@gmail.com>
2015-04-08 14:29:38 -0400

    Added contact.rb and have first/last name displaying properly.

--- crm.rb
+++ crm.rb
@@ -10,5 +10,11 @@ end
 
 get '/contacts' do
 	  @crm_app_name = "Dan's CRM"
+
+	  @contacts = []
+  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+
 	erb :contacts
 end
