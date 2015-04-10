d6df4d5be91d1d5268023b6f4aa9d10434707b65
Dan Comand <dcomand@gmail.com>
2015-04-10 16:24:53 -0400

    Added delete function, adding styling next.

--- crm.rb
+++ crm.rb
@@ -62,3 +62,15 @@ put "/contacts/:id" do
     raise Sinatra::NotFound
   end
 end
+
+delete "/contacts/:id" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    @@rolodex.remove_contact(@contact)
+    redirect to("/contacts")
+    # erb :delete
+  else
+    raise Sinatra::NotFound
+  end
+end
+
--- rolodex.rb
+++ rolodex.rb
@@ -17,5 +17,10 @@ class Rolodex
     @contacts.find {|contact| contact.id == contact_id.to_i }
   end
 
+    def remove_contact(contact)
+    @contacts.delete(contact)
+  end
+
+
 end
 
